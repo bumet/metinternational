7fddd219fdf38f62fbf054ff66156fcdda96f8f7
Richard Torres <rtorres@bu.edu>
2017-07-10 08:57:59 -0400

    Updates to CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,32 +2,40 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'css/style.css': 'css-dev/style.scss'
+        }
+      }
+    },
     concat: {
       dist: {
-        src: ['src/jquery-1.1.3.1.pack.js', 'src/thickbox.min.js', 'src/met-international.js'],
-        dest: 'dest/met-international.js'
+        src: ['js-dev/jquery-1.1.3.1.pack.js', 'js-dev/thickbox.min.js', 'js-dev/met-international.js'],
+        dest: 'js/met-international.js'
       }
     },
     uglify: {
       my_target: {
         files: {
-          'dest/met-international.min.js': ['dest/met-international.js']
+          'js/met-international.min.js': ['js/met-international.js']
         }
       }
     },
     watch: {
       scripts: {
-        files: ['src/*.js'],
-        tasks: ['concat', 'uglify']
+        files: ['js-dev/*.js', 'css-dev/style.scss'],
+        tasks: ['sass', 'concat', 'uglify']
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+  grunt.registerTask('default', ['sass', 'concat', 'uglify', 'watch']);
 
 };
